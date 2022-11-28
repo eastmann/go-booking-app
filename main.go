627abec4b373e17712bf7defdbf5e4d8e0a05998
Maxim Klochkov <maxim.klochkov@gmail.com>
2022-11-29 01:18:32 +0300

    Working with types

--- main.go
+++ main.go
@@ -3,12 +3,23 @@ package main
 import "fmt"
 
 func main() {
-	var conferenceName = "Go Conference"
-	const conferenceTickets = 50
-	var remainigTickets = 50
+	conferenceName := "Go Conference"
+	const conferenceTickets int = 50
+	var remainigTickets uint = 50
 
-	fmt.Println("Welcome to our", conferenceName, "booking application!")
-	fmt.Println("We have total of", conferenceTickets, "tickets and", remainigTickets, "are still available.")
+	// remainigTickets = -1
+
+	fmt.Printf("conferenceTickets is %T, remainigTickets is %T, conferenceName is %T \n", conferenceTickets, remainigTickets, conferenceName)
+
+	fmt.Printf("Welcome to our %q booking application! \n", conferenceName)
+	fmt.Printf("We have total of %v tickets and %v are still available. \n", conferenceTickets, remainigTickets)
 	fmt.Println("Get your tickets here to attend")
 
+	var userName string
+	var userTickets int
+	// ask user for his name
+
+	userName = "Tom"
+	userTickets = 2
+	fmt.Printf("User %q booked %v tickets.\n", userName, userTickets)
 }
