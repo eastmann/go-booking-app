861dd4a652023b50c2e9a1bf36e8f0188371d2cd
Maxim Klochkov <maxim.klochkov@gmail.com>
2022-11-29 01:36:04 +0300

    Adding user input

--- main.go
+++ main.go
@@ -13,13 +13,25 @@ func main() {
 
 	fmt.Printf("Welcome to our %q booking application! \n", conferenceName)
 	fmt.Printf("We have total of %v tickets and %v are still available. \n", conferenceTickets, remainigTickets)
-	fmt.Println("Get your tickets here to attend")
+	fmt.Println("Get your tickets here to attend.")
 
-	var userName string
+	var firstName string
+	var lastName string
+	var email string
 	var userTickets int
-	// ask user for his name
+	// Ask user for his name
 
-	userName = "Tom"
-	userTickets = 2
-	fmt.Printf("User %q booked %v tickets.\n", userName, userTickets)
+	fmt.Println("Please, enter your first name: ")
+	fmt.Scan(&firstName)
+
+	fmt.Println("Please, enter your last name: ")
+	fmt.Scan(&lastName)
+
+	fmt.Println("Please, enter your email: ")
+	fmt.Scan(&email)
+
+	fmt.Println("Please, enter number of tickets: ")
+	fmt.Scan(&userTickets)
+
+	fmt.Printf("Thank you, %v %v for booking %v tickets. You will receive a confirmation email at %v. \n", firstName, lastName, userTickets, email)
 }
